e689a1ad6438ce06ceb31ddfab3d3d663fe7e04f
mkaraev <maruf.karaev97@gmail.com>
2023-02-19 16:34:24 +0500

    Add first script and linter to check formatting

---
+++ Makefile
@@ -0,0 +1,15 @@
+install:
+	npm ci
+
+
+brain-games:
+	node bin/brain-games.js
+
+publish:
+	npm publish --dry-run
+
+lint:
+	npx eslint .
+
+lint-fix:
+	 npx eslint --fix .
---
+++ bin/brain-games.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+const welcomeUser = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+};
+
+welcomeUser();
